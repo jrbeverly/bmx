41deb2185e3a2b754eaa18795da133045790fa0a
Jonathan Beverly <jonathan.beverly@arcticwolf.com>
2020-10-13 09:47:30 -0400

    Split out verify actions to funcs

--- saml/identityProviders/okta/okta.go
+++ saml/identityProviders/okta/okta.go
@@ -266,6 +266,47 @@ func removeExpiredOktaSessions(sourceCaches []file.OktaSessionCache) []file.Okta
 	return returnCache
 }
 
+func (o *OktaClient) verifyPushMfa(oktaAuthResponse *OktaAuthResponse, selectedFactor OktaAuthFactors) error {
+	verified := false
+	for !verified {
+		body := fmt.Sprintf(`{"stateToken":"%s"}`, oktaAuthResponse.StateToken)
+		authResponse, err := o.HttpClient.Post(selectedFactor.Links.Verify.Url, "application/json", strings.NewReader(body))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		z, _ := ioutil.ReadAll(authResponse.Body)
+		if err := json.Unmarshal(z, &oktaAuthResponse); err != nil {
+			log.Fatal(err)
+		}
+
+		if oktaAuthResponse.Status == "SUCCESS" {
+			verified = true
+		} else if oktaAuthResponse.Status == "MFA_CHALLENGE" || oktaAuthResponse.Status == "WAITING" {
+			time.Sleep(8 * time.Second)
+		}
+	}
+}
+
+func (o *OktaClient) verifyTotpMfa(oktaAuthResponse *OktaAuthResponse, selectedFactor OktaAuthFactors) error {
+	var code string
+	if code, err := o.ConsoleReader.ReadLine("Code: "); err != nil {
+		log.Fatal(err)
+	}
+	body := fmt.Sprintf(`{"stateToken":"%s","passCode":"%s"}`, oktaAuthResponse.StateToken, code)
+	authResponse, err := o.HttpClient.Post(selectedFactor.Links.Verify.Url, "application/json", strings.NewReader(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	z, _ := ioutil.ReadAll(authResponse.Body)
+	if err := json.Unmarshal(z, &oktaAuthResponse); err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
 func (o *OktaClient) doMfa(oktaAuthResponse *OktaAuthResponse) error {
 	if oktaAuthResponse.Status == "MFA_REQUIRED" {
 		fmt.Fprintln(os.Stderr, "MFA Required")
@@ -294,39 +335,14 @@ func (o *OktaClient) doMfa(oktaAuthResponse *OktaAuthResponse) error {
 		}
 
 		if selectedFactor.FactorType == "token:software:totp" {
-			var code string
-			if code, err = o.ConsoleReader.ReadLine("Code: "); err != nil {
-				log.Fatal(err)
-			}
-			body = fmt.Sprintf(`{"stateToken":"%s","passCode":"%s"}`, oktaAuthResponse.StateToken, code)
-			authResponse, err = o.HttpClient.Post(vurl, "application/json", strings.NewReader(body))
+			err = o.verifyTotpMfa(oktaAuthResponse, selectedFactor)
 			if err != nil {
 				log.Fatal(err)
 			}
-
-			z, _ = ioutil.ReadAll(authResponse.Body)
-			if err := json.Unmarshal(z, &oktaAuthResponse); err != nil {
-				log.Fatal(err)
-			}
 		} else if selectedFactor.FactorType == "push" {
-			verified := false
-			for !verified {
-				body = fmt.Sprintf(`{"stateToken":"%s"}`, oktaAuthResponse.StateToken)
-				authResponse, err = o.HttpClient.Post(vurl, "application/json", strings.NewReader(body))
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				z, _ = ioutil.ReadAll(authResponse.Body)
-				if err := json.Unmarshal(z, &oktaAuthResponse); err != nil {
-					log.Fatal(err)
-				}
-
-				if oktaAuthResponse.Status == "SUCCESS" {
-					verified = true
-				} else if oktaAuthResponse.Status == "MFA_CHALLENGE" || oktaAuthResponse.Status == "WAITING" {
-					time.Sleep(8 * time.Second)
-				}
+			err = o.verifyTotpMfa(oktaAuthResponse, selectedFactor)
+			if err != nil {
+				log.Fatal(err)
 			}
 		}
 	}
