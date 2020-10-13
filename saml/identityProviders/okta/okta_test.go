f821dbff4e71c2893c2752b16a1bb748006ca35c
Jonathan Beverly <jrbeverly@live.com>
2020-10-13 17:38:35 -0400

    Test adding in mocks

--- saml/identityProviders/okta/okta_test.go
+++ saml/identityProviders/okta/okta_test.go
@@ -24,6 +24,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/jrbeverly/bmx/mocks"
 	"github.com/jrbeverly/bmx/saml/identityProviders/okta"
 	"github.com/jrbeverly/bmx/saml/identityProviders/okta/file"
 	"github.com/jrbeverly/bmx/saml/identityProviders/okta/mocks"
